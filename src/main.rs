aca2c90abd55799dfdea057fd0901d99af8db27e
Joshua Holmes <JoshuaDanielHolmes1991@gmail.com>
2016-05-12 15:00:13 -0500

    Reading in a few fields from the header. This will all be properly modularized once it works well.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::str;
 
 fn main() {
     // get a file handle to our ROM
@@ -9,26 +10,51 @@ fn main() {
     let display = path.display();
 
     let mut file = match File::open(&path) {
-        Err(why) => {
-            panic!("Couldn't open {}. Error message: {}", display, Error::description(&why))
-        },
-        Ok(file) => {
-            println!("File handle to the ROM opened successfully."); 
-            file
-        },
+        Err(why) => panic!("Couldn't open {}. Error message: {}", display, Error::description(&why)),
+        Ok(file) => file,
     };
 
     // read the ROM into a buffer
     let mut buf = Vec::new();
 
     match file.read_to_end(&mut buf) {
-        Err(why) => {
-            panic!("Couldn't read {}. Error message: {}", display, Error::description(&why))
-        },
-        Ok(_) => {
-            println!("ROM file read successfully. Length: {}", buf.len())
-        },
+        Err(why) => panic!("Couldn't read {}. Error message: {}", display, Error::description(&why)),
+        Ok(_) => println!("{} read successfully. Length: {}", display, buf.len()),
     }
 
     // start reading information about the ROM
+
+    // title
+    let title_bytes = &buf[0x0134..0x013F];
+    let title = match str::from_utf8(title_bytes) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    println!("\nTitle bytes: {:?}", title_bytes);
+    println!("Title: {}", title);
+
+    // manufacturer code
+    let manufacturer_code_bytes = &buf[0x013F..0x0143];
+    let manufacturer_code = match str::from_utf8(manufacturer_code_bytes) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    println!("\nManufacturer code bytes: {:?}", manufacturer_code_bytes);
+    println!("Manufacturer code: {}", manufacturer_code);
+
+    // CGB flag
+    let cgb_flag = buf[0x0143];
+    println!("\nCGB flag: 0x{:X}", cgb_flag);
+
+    // new licensee code
+    let new_licensee_code_bytes = &buf[0x0144..0x0146];
+    let new_licensee_code = match str::from_utf8(new_licensee_code_bytes) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    println!("\nNew licensee code bytes: {:?}", new_licensee_code_bytes);
+    println!("New licensee code: {}", new_licensee_code);
 }
